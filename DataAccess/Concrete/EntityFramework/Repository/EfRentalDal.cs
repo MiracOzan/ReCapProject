5d0fcc02a3b2485e91d076a9aa57700d2ebd89fd
Miraç Ozan T <m.ozan.tosun@gmail.com>
2022-08-17 17:56:22 +0300

    İmplements entity framework repository design

---
+++ DataAccess/Concrete/EntityFramework/Repository/EfRentalDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq.Expressions;
+using DataAccess.Concrete.EntityFramework.Contexts;
+
+namespace DataAccess.Concrete.EntityFramework.Repository
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, RentACarContext>, IRentalDal
+    {
+
+    }
+}
